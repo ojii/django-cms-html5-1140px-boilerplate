0a59abee412f328827149f1a34f74e428933a496
MARTIN_BROCHHAUS <MARTIN.BROCHHAUS@ORBIT.DE>
2010-12-20 02:14:51 +0800

    added THUMBNAIL_PROCESSORS to settings.py

--- webapps/django/project/settings.py
+++ webapps/django/project/settings.py
@@ -92,6 +92,14 @@ MIDDLEWARE_CLASSES = (
     #'django.middleware.cache.FetchFromCacheMiddleware',
 )
 
+THUMBNAIL_PROCESSORS = (
+    'easy_thumbnails.processors.colorspace',
+    'easy_thumbnails.processors.autocrop',
+    #'easy_thumbnails.processors.scale_and_crop',
+    'filer.thumbnail_processors.scale_and_crop_with_subject_location',
+    'easy_thumbnails.processors.filters',
+)
+
 ROOT_URLCONF = 'project.urls'
 
 CMS_TEMPLATES = (
