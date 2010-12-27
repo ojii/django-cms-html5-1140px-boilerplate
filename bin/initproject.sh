73c0dba13432f6ff7e5f722a9a827384c6c0103a
Martin Brochhaus <mbrochh@gmail.com>
2010-12-27 02:03:18 +0100

    create superuser / run migrations / runserver

--- bin/initproject.sh
+++ bin/initproject.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
-echo "Updating git submodules"
+source /usr/local/bin/virtualenvwrapper.sh
+echo "Updating git submodules..."
 git submodule update --init --recursive
 
-echo "Creating static folders"
+echo "Creating static folders..."
 mkdir ./webapps/static
 mkdir ./webapps/static/img
 mkdir ./webapps/static/css
 mkdir ./webapps/static/js
 touch ./webapps/static/css/styles.sass
 
-echo "Copying the parts of html5-boilerplate that we need"
+echo "Copying the parts of html5-boilerplate that we need..."
 cp ./lib/html5-boilerplate/404.html ./webapps/django/project/templates/404.html
 cp ./lib/html5-boilerplate/apple-touch-icon.png ./webapps/static/img/apple-touch-icon.png
 cp ./lib/html5-boilerplate/favicon.ico ./webapps/static/img/favicon.ico
@@ -17,11 +18,11 @@ cp ./lib/html5-boilerplate/robots.txt ./webapps/static/robots.txt
 cp -r ./lib/html5-boilerplate/js ./webapps/static/
 cp -r ./lib/html5-boilerplate/css ./webapps/static/
 
-echo "Copying the parts of 1140px css grid that we need"
+echo "Copying the parts of 1140px css grid that we need..."
 cp ./lib/1140px/1140.css ./webapps/static/css/1140.css
 cp ./lib/1140px/ie.css ./webapps/static/css/ie.css
 
-echo "Splitting up html5-boilerplate css"
+echo "Splitting up html5-boilerplate css..."
 SPLIT=`grep -n "/* Primary Styles" ./webapps/static/css/style.css | awk -F":" '{ print $1 }'`
 SPLITHEAD=`expr $SPLIT - 1`
 SPLITTAIL=`expr $SPLIT + 3`
@@ -33,7 +34,7 @@ rm -rf .git
 rm README.rst
 rm ./webapps/static/css/style.css
 
-echo "Creating symlinks"
+echo "Creating symlinks..."
 cd webapps/static
 echo "What is the name of your virtualenv: "
 read virtualenvname
@@ -43,7 +44,7 @@ ln -s $HOME/Envs/$virtualenvname/src/filer/filer/media/filer
 ln -s $HOME/Envs/$virtualenvname/src/multilingual/multilingual/media/multilingual
 cd ../..
 
-echo "Creating local_settings.py"
+echo "Creating local_settings.py ..."
 cd webapps/django/project/
 cp local_settings.py.sample local_settings.py
 echo "What is your database username: "
@@ -58,3 +59,9 @@ git init
 git add .
 git commit -m "Initial Commit"
 
+echo "Initializing database..."
+cd webapps/django/project/
+workon $virtualenvname
+python manage.py syncdb
+python manage.py migrate
+python manage.py runserver
