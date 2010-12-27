6851f863cc3d2b4b594b6f5cd5d804004ff93813
Martin Brochhaus <mbrochh@gmail.com>
2010-12-27 01:37:50 +0100

    fixed cutoff lines

--- bin/initproject.sh
+++ bin/initproject.sh
@@ -37,10 +37,10 @@ echo "Creating symlinks"
 cd webapps/static
 echo "What is the name of your virtualenv: "
 read virtualenvname
-ln -s $HOME/Envs/$virtualenvname/lib/python2.7/site-packages/django/contrib/adm$
+ln -s $HOME/Envs/$virtualenvname/lib/python2.7/site-packages/django/contrib/admin/media
 ln -s $HOME/Envs/$virtualenvname/src/django-cms/cms/media/cms
 ln -s $HOME/Envs/$virtualenvname/src/filer/filer/media/filer
-ln -s $HOME/Envs/$virtualenvname/src/multilingual/multilingual/media/multilingu$
+ln -s $HOME/Envs/$virtualenvname/src/multilingual/multilingual/media/multilingual
 cd ../..
 
 echo "Creating local_settings.py"
